106908f6406c6b89d011898c62568f4b957fc5c9
Kerry Zhang <kerryzhang12@gmail.com>
2026-04-14 20:36:01 -0400

    properly updated font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['"JetBrains Mono"', 'monospace'],
+      },
       animation: {
         float: "float ease-in-out infinite",
       },
